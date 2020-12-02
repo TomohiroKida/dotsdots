65c2e609283722b0a5fd258a8f4bc0506dfb0df8
kida <tomo426.sakura1281m@gmail.com>
2020-12-02 19:28:36 +0900

    create setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat README.md
+
+ln -s bashrc ~/.bashrc
+ln -s emacs ~/.emacs
+ln -s inputrc ~/.inputrc
+ln -s screenrc ~/.screenrc
+ln -s tmux.conf ~/.tmux.conf
+ln -s vimrc ~/.vimrc
